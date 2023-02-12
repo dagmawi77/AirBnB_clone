779ede2030616bebf46d1498ddb56acbae5c1b65
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:25:43 +0300

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for models/state.py.
-
-Unittest classes:
-    TestState_instantiation
-    TestState_save
-    TestState_to_dict
 """
 import os
 import models
