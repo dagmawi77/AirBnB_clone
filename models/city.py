497ea73cf1dfe8f0271af5fe17a1eb8d1f5996b1
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:27:44 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -5,10 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """Represent a city.
-
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
     """
 
     state_id = ""
