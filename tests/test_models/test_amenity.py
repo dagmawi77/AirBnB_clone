25f3cd6556a336eb3a46d3eee1251d9dd6ed4f4a
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:23:31 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for models/amenity.py.
-
-Unittest classes:
-    TestAmenity_instantiation
-    TestAmenity_save
-    TestAmenity_to_dict
 """
 import os
 import models
