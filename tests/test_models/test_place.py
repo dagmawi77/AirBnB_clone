9a9a30071f5a35f471ca4a46f24db216fc7a1611
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:24:57 +0300

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for models/place.py.
-
-Unittest classes:
-    TestPlace_instantiation
-    TestPlace_save
-    TestPlace_to_dict
 """
 import os
 import models
