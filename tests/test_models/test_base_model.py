60406c4312ad758303aedace7c515f48f2cc4597
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:24:00 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py.
-
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
 """
 import os
 import models
