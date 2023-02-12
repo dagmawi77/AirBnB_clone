b9828448ca2b2774739f2a4825087c43402834f6
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:27:09 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,9 +5,6 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Represent an amenity.
-
-    Attributes:
-        name (str): The name of the amenity.
     """
 
     name = ""
