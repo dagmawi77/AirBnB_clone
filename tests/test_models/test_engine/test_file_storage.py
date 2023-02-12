8b11ea07a24aaf48d39198c2291070d05a3c6a1b
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-02-12 20:26:42 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for models/engine/file_storage.py.
-
-Unittest classes:
-    TestFileStorage_instantiation
-    TestFileStorage_methods
 """
 import os
 import json
